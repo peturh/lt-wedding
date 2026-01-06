b1ab32951e7fd7661124567cb7306b330229b006
petur <petur.hjartarson@gmail.com>
2026-01-06 11:22:50 +0100

    fix: moved to google sheet

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,37 +1,34 @@
-import prisma from '$lib/prisma';
+import { addGuest } from '$lib/sheets';
 import { env } from '$env/dynamic/private';
-
 
 import type { Actions } from './$types';
 
 export const actions = {
-    default: async ({ request }) => {
-        const data = await request.formData();
-        let password = data.get('password')
-        console.log(env.FORM_PASSWORD, password)
-        let name = data.get("name") || ''
-        let phoneNumber = data.get("phoneNumber") || ''
-        let email = data.get("email") || ''
-        let foodPreference = data.get("foodPreference") || ''
-        if (password == env.FORM_PASSWORD) {
-            await prisma.guest.create({
-                data: {
-                    name: name.toString(),
-                    phoneNumber: phoneNumber.toString(),
-                    email: email.toString(),
-                    foodPreference: foodPreference.toString()
-                }
-            });
-            return { success: true, person: name.toString() };
-        } else {
-            return {
-                wrongPassword: true,
-                name: name.toString(),
-                phoneNumber: phoneNumber.toString(),
-                email: email.toString(),
-                foodPreference: foodPreference.toString(),
-                password: password?.toString() ?? ''
-            }
-        }
-    }
+	default: async ({ request }) => {
+		const data = await request.formData();
+		let password = data.get('password');
+		let name = data.get('name') || '';
+		let phoneNumber = data.get('phoneNumber') || '';
+		let email = data.get('email') || '';
+		let foodPreference = data.get('foodPreference') || '';
+
+		if (password == env.FORM_PASSWORD) {
+			await addGuest({
+				name: name.toString(),
+				phoneNumber: phoneNumber.toString(),
+				email: email.toString(),
+				foodPreference: foodPreference.toString()
+			});
+			return { success: true, person: name.toString() };
+		} else {
+			return {
+				wrongPassword: true,
+				name: name.toString(),
+				phoneNumber: phoneNumber.toString(),
+				email: email.toString(),
+				foodPreference: foodPreference.toString(),
+				password: password?.toString() ?? ''
+			};
+		}
+	}
 } satisfies Actions;
